68c230b9c5a7c71ad004790efadfb6ab196cd6cb
Bulaev Denis <bulaevdenis7@gmail.com>
2021-07-09 14:27:14 +0300

    readme updated

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <button type="button" className="button is-primary is-large">
+        hi
+      </button>
+    </div>
+  );
+}
+
+export default App;
